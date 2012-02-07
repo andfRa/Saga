fb5240f78bbfb28e60cd4ffbb725b25c1295f88a
andf54 <andrjus@hotmail.com>
2012-02-07 22:06:27 +0200

    Added stoneskin ability.

---
+++ src/org/saga/abilities/Stoneskin.java
@@ -0,0 +1,300 @@
+package org.saga.abilities;
+
+import java.util.Random;
+
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.entity.Creature;
+import org.bukkit.entity.Projectile;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.util.Vector;
+import org.saga.Saga;
+import org.saga.player.PlayerMessages;
+import org.saga.player.SagaEntityDamageManager;
+import org.saga.player.SagaPlayer;
+import org.saga.player.Skill.ArmourType;
+import org.saga.statistics.StatisticsManager;
+import org.saga.utility.TextUtil;
+
+public class Stoneskin extends Ability{
+
+	
+	/**
+	 * The amount of uses the ability has.
+	 */
+	private Integer uses = null;
+	
+	
+	// Initialization:
+	/**
+	 * Initializes using definition.
+	 * 
+	 * @param definition ability definition
+	 */
+	public Stoneskin(AbilityDefinition definition) {
+		
+		super(definition);
+		this.uses = 0;
+       
+	}
+
+	// Uses:
+	/**
+	 * Called when the ability was used.
+	 */
+	private void use() {
+
+		if(this.uses == null) this.uses = 0;
+		
+		this.uses++;
+		
+	}
+	
+	/**
+	 * Clears the uses.
+	 * 
+	 * @return uses
+	 */
+	private int clearUses() {
+
+		if(this.uses == null) return 0;
+		
+		Integer uses = this.uses;
+		
+		this.uses = null;
+		
+		return uses;
+		
+	}
+	
+	// Events:
+	/* 
+	 * (non-Javadoc)
+	 * @see org.saga.abilities.Ability#onActivate()
+	 */
+	@Override
+	public boolean onPreActivate() {
+
+
+		// Heavy armour restriction:
+		if(getSagaPlayer().getArmor(ArmourType.HEAVY) > 0){
+			getSagaPlayer().message(invalidArmour(ArmourType.HEAVY));
+			return false;
+		}
+
+		// Exotic armour restriction:
+		if(getSagaPlayer().getArmor(ArmourType.EXOTIC) > 0){
+			getSagaPlayer().message(invalidArmour(ArmourType.EXOTIC));
+			return false;
+		}
+
+		// Play effect:
+		playeEffect(getSagaPlayer());
+	
+		// Reset uses:
+		this.uses = null;
+		
+		return true;
+		
+		
+	}
+	
+	/* 
+	 * (non-Javadoc)
+	 * @see org.saga.abilities.Ability#onPreDeactivate()
+	 */
+	@Override
+	public boolean onPreDeactivate() {
+		
+		
+		// Award exp:
+		Integer awardedExp = awardExperience(clearUses());
+		
+		// Statistics:
+		StatisticsManager.manager().onAbilityUse(getName(), awardedExp);
+		
+		return true;
+		
+		
+	}
+	
+	/* 
+	 * (non-Javadoc)
+	 * @see org.saga.abilities.Ability#onHitByPlayer(org.bukkit.event.entity.EntityDamageByEntityEvent, org.saga.player.SagaPlayer)
+	 */
+	@Override
+	public boolean onHitByPlayer(EntityDamageByEntityEvent event, SagaPlayer attacker) {
+
+	
+		// Check pre use:
+		if(!handlePreUse()){
+			return false;
+		}
+
+		return handleAbility(event);
+		
+	
+	}
+	
+	/* 
+	 * (non-Javadoc)
+	 * @see org.saga.abilities.Ability#onHitByCreature(org.bukkit.event.entity.EntityDamageByEntityEvent, org.bukkit.entity.Creature)
+	 */
+	@Override
+	public boolean onHitByCreature(EntityDamageByEntityEvent event, Creature creature) {
+
+		
+		// Check pre use:
+		if(!handlePreUse()){
+			return false;
+		}
+
+		return handleAbility(event);
+		
+	
+	}
+	
+	/* 
+	 * (non-Javadoc)
+	 * @see org.saga.abilities.Ability#onShotByPlayer(org.bukkit.event.entity.EntityDamageByEntityEvent, org.saga.player.SagaPlayer, org.bukkit.entity.Projectile)
+	 */
+	@Override
+	public boolean onShotByPlayer(EntityDamageByEntityEvent event, SagaPlayer attacker, Projectile projectile) {
+
+		
+		// Check pre use:
+		if(!handlePreUse()){
+			return false;
+		}
+
+		return handleAbility(event);
+		
+	
+	}
+	
+	/* 
+	 * (non-Javadoc)
+	 * @see org.saga.abilities.Ability#onShotCreature(org.bukkit.event.entity.EntityDamageByEntityEvent, org.bukkit.entity.Creature, org.bukkit.entity.Projectile)
+	 */
+	@Override
+	public boolean onShotByCreature(EntityDamageByEntityEvent event, Creature creature, Projectile projectile) {
+
+		
+		// Check pre use:
+		if(!handlePreUse()){
+			return false;
+		}
+
+		return handleAbility(event);
+		
+	
+	}
+	
+	/* 
+	 * (non-Javadoc)
+	 * @see org.saga.abilities.Ability#onSpelledByPlayer(org.bukkit.event.entity.EntityDamageByEntityEvent, org.saga.player.SagaPlayer, org.bukkit.entity.Projectile)
+	 */
+	@Override
+	public boolean onSpelledByPlayer(EntityDamageByEntityEvent event, SagaPlayer defender, Projectile projectile) {
+
+//		
+//		// Check pre use:
+//		if(!handlePreUse()){
+//			return false;
+//		}
+//
+//		return handleAbility(event);
+//		
+	
+		
+		return false;
+		
+	}
+	
+	/**
+	 * Handles ability.
+	 * 
+	 * @param event event
+	 * @return true if successful
+	 */
+	private boolean handleAbility(EntityDamageByEntityEvent event) {
+
+
+		// Heavy armour restriction:
+		if(getSagaPlayer().getArmor(ArmourType.HEAVY) > 0){
+			getSagaPlayer().message(invalidArmour(ArmourType.HEAVY));
+			return false;
+		}
+
+		// Exotic armour restriction:
+		if(getSagaPlayer().getArmor(ArmourType.EXOTIC) > 0){
+			getSagaPlayer().message(invalidArmour(ArmourType.EXOTIC));
+			return false;
+		}
+
+		Double damageMult = 1 - getDefinition().getPrimaryFunction().calculateValue(getSkillLevel());
+
+		// Normalize:
+		if(damageMult < 0){
+			Saga.severe(this, "damage multiplier negative", "using 0.0");
+			damageMult = 0.0;
+		}
+		else if(damageMult > 1){
+			Saga.severe(this, "damage multiplier over 1", "using 1.0");
+			damageMult = 1.0;
+		}
+		
+		// Reduce damage;
+		event.setDamage(SagaEntityDamageManager.randomRound(event.getDamage() * damageMult));
+
+		// Play effect:
+		playeEffect(getSagaPlayer());
+		
+		// Use:
+		use();
+		
+		return true;
+		
+		
+	}
+	
+	/**
+	 * Plays ability effect.
+	 * 
+	 * @param sagaPlayer saga player
+	 */
+	private static void playeEffect(SagaPlayer sagaPlayer) {
+
+		Location feetLoc = sagaPlayer.getLocation();
+		
+		if(feetLoc == null) return;
+		
+		Location headLoc = sagaPlayer.getLocation();
+		headLoc.add(0, 1, 0);
+		
+		// Play effect:
+		sagaPlayer.playGlobalEffect(Effect.STEP_SOUND, 1, feetLoc);
+		sagaPlayer.playGlobalEffect(Effect.STEP_SOUND, 1, headLoc);
+		
+		
+	}
+	
+	public String invalidArmour(ArmourType type){
+		
+		switch (type) {
+		case HEAVY:
+			
+			return PlayerMessages.negative + TextUtil.capitalize(getName()) + " can't be used with heavy armour.";
+
+		case EXOTIC:
+	
+			return PlayerMessages.negative + TextUtil.capitalize(getName()) + " can't be used with exotic armour.";
+
+		default:
+			return PlayerMessages.negative + "Invalid armour.";
+		}
+		
+	}
+	
+	
+}
