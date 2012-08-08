6391fdcc1eab5ca65b17b89bdaa8027a097e3e4d
andfRa <andrjus@hotmail.com>
2012-08-08 17:41:34 +0300

    Tweaked fstats.

--- src/org/saga/messages/FactionMessages.java
+++ src/org/saga/messages/FactionMessages.java
@@ -574,7 +574,7 @@ public static ChatColor positiveHighlightColor = ChatColor.GREEN;
 				String name = settlements[i].getName();
 				Location location = retTownSquareLoc(settlements[i]);
 				
-				String locationStr = "none";
+				String locationStr = "no " + InfoMessages.townSquare();
 				if(location != null) locationStr = location(location);
 				
 				String closestName = "none";
